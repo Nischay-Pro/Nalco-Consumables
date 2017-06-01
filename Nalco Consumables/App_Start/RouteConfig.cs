ad050ee4063d24bc4099a882e34aad1b5788e75b
Nischay Pro <NischayPro@gmail.com>
2017-06-01 11:33:38 +0530

    Added Skeleton

--- Nalco Consumables/App_Start/RouteConfig.cs
+++ Nalco Consumables/App_Start/RouteConfig.cs
@@ -15,8 +15,7 @@ namespace Nalco_Consumables
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "index.html"
             );
         }
     }
