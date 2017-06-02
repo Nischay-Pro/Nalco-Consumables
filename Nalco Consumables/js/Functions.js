c1b531e420dd8295b6f31d5494cdcd4e0b707aef
Nischay Pro <NischayPro@gmail.com>
2017-06-02 09:33:01 +0530

    Before Deploying to Server

---
+++ Nalco Consumables/js/Functions.js
@@ -0,0 +1,32 @@
+﻿function ShowMaterial() {
+    SuperHide("materials-main");
+};
+function ShowIssue() {
+    SuperHide("issues-main");
+};
+function ShowPO() {
+    SuperHide("po-main");
+};
+function ShowReport() {
+    SuperHide("report-main");
+};
+function switchTo(b,a) {
+    var obj = document.getElementById(a);
+    obj.style.display = "none";
+    var obj = document.getElementById(b);
+    obj.style.display = "block";
+};
+
+var sectionsthere = [
+    "materials-main", "issues-main", "po-main", "report-main", "materials-create"
+];
+function SuperHide(antihide) {
+    sectionsthere.forEach(function (element) {
+        if (antihide != element) {
+            var obj = document.getElementById(element);
+            obj.style.display = "none";
+        }
+    })
+    var obj = document.getElementById(antihide);
+    obj.style.display = "block";;
+};
