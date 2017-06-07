c813e6aa9a20987b0808fc42fe1e978c9b674ff6
Nischay Pro <NischayPro@gmail.com>
2017-06-07 16:16:09 +0530

    Retrieval of Materials

--- Nalco Consumables/js/functions.js
+++ Nalco Consumables/js/functions.js
@@ -1,15 +1,33 @@
 ﻿var username, password;
+if (getCookie('username') === "") {
+}
+else {
+    username = getCookie('username');
+    password = getCookie('password');
+    CheckUser(false);
+}
 function ShowMain() {
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
-    CheckUser();
+    CheckUser(true);
 }
 function ShowMaterialsMain() {
     document.getElementById("dashboard-nav").style.display = "none";
     document.getElementById("materials-main").style.display = "block";
+    MaterialsList();
+}
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
 function LogOut() {
-    document.cookie = "";
+    deleteAllCookies();
     document.getElementById("dashboard").style.display = "none";
     document.getElementById("logout").style.display = "block";
 }
@@ -33,7 +51,7 @@ function getCookie(cname) {
     }
     return "";
 }
-function CheckUser() {
+function CheckUser(first) {
     var authorizationBasic = window.btoa(username + ':' + password);
     var request = new XMLHttpRequest();
     request.open('POST', 'api/Users', true);
@@ -46,15 +64,77 @@ function CheckUser() {
             if (request.responseText === "\"Authentication Success\"") {
                 document.getElementById("dashboard").style.display = "block";
                 document.getElementById("login").style.display = "none";
-                setCookie("username", document.getElementById("username").value, 1);
-                setCookie("password", document.getElementById("password").value, 1);
+                if (first == true) {
+                    setCookie("username", document.getElementById("username").value, 1);
+                    setCookie("password", document.getElementById("password").value, 1);
+                }
                 document.getElementById("username_holder").innerText = getCookie("username");
                 document.getElementById("invalidcreds").style.display = "none";
             }
             else {
                 document.getElementById("invalidcreds").style.display = "block";
-                 }
+            }
         }
-
     };
+}
+function MaterialsList() {
+    var authorizationBasic = window.btoa(username + ':' + password);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'api/Materials', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
+    request.setRequestHeader('Accept', 'application/json');
+    request.send();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && JSON.parse(request.responseText).message != 'Authorization has been denied for this request.') {
+            var data = JSON.parse(request.responseText);
+            data = data.replace(/\s/g, "");
+            data = JSON.parse(data);
+            $('#materialslist').bootstrapTable({
+                columns: [{
+                    field: 'material_code',
+                    title: 'Material Code'
+                }, {
+                    field: 'material_description',
+                    title: 'Material Description'
+                }, {
+                    field: 'material_printer',
+                    title: 'Material Printer'
+                }, {
+                    field: 'material_printer_description',
+                    title: 'Material Printer Description'
+                }, {
+                    field: 'material_printer_count',
+                    title: 'Material Printer Count'
+                }, {
+                    field: 'material_quantity',
+                    title: 'Material Quantity'
+                }, {
+                    field: 'material_critical_flag',
+                    title: 'Material Critical Flag'
+                }, {
+                    field: 'material_reorder_level',
+                    title: 'Material Reorder Level'
+                }, {
+                    field: 'material_storage',
+                    title: 'Material Storage'
+                }],
+                data: data
+            });
+            //var test, i;
+            //i = 0;
+            //do {
+            //   if (data[i] === undefined) {
+            //      break;
+            // }
+            //var x = document.getElementById('materialslist').insertRow(-1);
+            //var c1 = x.insertCell(0);
+            //var c2 = x.insertCell(1);
+            //c1.innerHTML = data[i].material_code;
+            //c2.innerHTML = data[i].material_description;
+            //i++;
+            //}
+            //while (data[i] !== undefined && data[i] !== null);
+        }
+    }
 }
