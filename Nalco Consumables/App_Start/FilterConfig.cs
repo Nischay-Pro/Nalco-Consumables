cdc440cb9be3405d3d37233b95e0e7a17a144d9c
Nischay Pro <NischayPro@gmail.com>
2017-06-28 23:21:26 +0530

    Added Application Insights

--- Nalco Consumables/App_Start/FilterConfig.cs
+++ Nalco Consumables/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace Nalco_Consumables
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
